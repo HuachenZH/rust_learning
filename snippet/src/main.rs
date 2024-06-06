47e9b05943d2c80d9c814b321cf6aa5154aa4a90
HuachenZH <huachen.zhang.23@eigsi.fr>
2024-06-06 15:38:48 +0200

    initial commit snippet

---
+++ snippet/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let mut x = 5;
+    x = x +1;
+    println!("The value of x is {x}");
+
+    // ------------------------
+    let x = 5;
+    let x = x+1;
+    println!("The value of x is {x}");
+
+    // ------------------------
+    // can i get a variable from inner scope?
+    //{
+    //    let _x = 5;
+    //}
+    //println!("The value of _x is {_x}");
+    // no i can't'
+
+
+}
